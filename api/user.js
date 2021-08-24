230719efd5e3a85726098d71d8609a905069a4c6
Bruno Moraes <brunodelciomoraes@gmail.com>
2021-08-23 22:48:16 -0300

    Estruturando o projeto
    instalação de dependências e criação as rotas básicas do projeto.

---
+++ api/user.js
@@ -0,0 +1,7 @@
+module.exports = app => {
+    const save = (req, res) => {
+        res.send('this user')
+    }
+
+    return {save}
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')()
+const consign = require('consign');
+
+consign()
+    .then('config/middlewares.js')
+    .then('api')
+    .then('config/routes.js')
+    .into(app)
+
+app.listen(4000, () => {
+    console.log("Executando...");
+})
+
