5f9215bf2aad04374688fa8b1f793c9114156bcd
Bruno Moraes <brunodelciomoraes@gmail.com>
2021-08-24 23:19:06 -0300

    API de usuários

--- api/user.js
+++ api/user.js
@@ -1,7 +1,79 @@
+const bcrypt = require('bcrypt');
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('this user')
+    const { existsOrError, notExistsOrError, equalsOrError, minPassword } = app.api.validator
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
     }
 
-    return {save}
+    const save = async (req, res) => {
+        const user = { ...req.body }
+        if (req.params.id) user.id = req.params.id
+
+        try {
+            existsOrError(user.nome, 'Nome não informado')
+            existsOrError(user.email, 'E-mail não informado')
+            existsOrError(user.senha, 'Senha não informada')
+            existsOrError(user.confirmSenha, 'Confirmação de Senha inválida')
+            equalsOrError(user.senha, user.confirmSenha, 'Senhas não conferem')
+            minPassword(user.senha, 'Senha precisa ter no mínimo 6 caracteres')
+
+            const userFromDB = await app.db('users')
+                .where({ email: user.email }).first()
+            if (!user.id) {
+                notExistsOrError(userFromDB, 'Usuário já cadastrado!')
+            }
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+        user.password = encryptPassword(user.senha)
+        delete user.confirmSenha
+
+        if (user.id) {
+            app.db('users')
+                .update(user)
+                .where({ id: user.id })
+                .whereNull('deletedAt')
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('users')
+                .insert(user)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+
+    const getById = (req, res) => {
+        app.db('users')
+            .select('id', 'nome', 'email')
+            .where({ id: req.params.id })
+            .whereNull('deletedAt')
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const remove = async (req, res) => {
+        try {
+            const daily = await app.db('daily')
+                .where({ userId: req.params.id })
+            notExistsOrError(daily, 'Usuário possui diários.')
+
+            const rowsUpdated = await app.db('users')
+                .update({ deletedAt: new Date() })
+                .where({ id: req.params.id })
+            existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
+
+            res.status(204).send()
+        } catch (msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, getById, remove }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
+
+    app.route('/users/:id')
+        .put(app.api.user.save)
 }
